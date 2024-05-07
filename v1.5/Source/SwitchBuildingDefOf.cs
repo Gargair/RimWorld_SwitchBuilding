bfe53395198ed04a8522e09bc22a3deb27b9562c
Gargair <Gargair@users.noreply.github.com>
2024-05-07 08:26:35 +0200

    Removed obsolete stuff

--- v1.5/Source/SwitchBuildingDefOf.cs
+++ v1.5/Source/SwitchBuildingDefOf.cs
@@ -6,18 +6,6 @@ namespace SwitchBuilding
     public static class SwitchBuildingDefOf
     {
         [DefOf]
-        public static class JobDefs
-        {
-            public static JobDef ChangeBuilding;
-        }
-
-        [DefOf]
-        public static class WorkGivers
-        {
-            public static WorkGiverDef ChangeBuilding;
-        }
-
-        [DefOf]
         public static class Designations
         {
             public static DesignationDef ChangeBuilding;
